91dfe448ac3535fe1c84c7b884b548a5129fe867
Bayan <bsobhani@email.arizona.edu>
2016-04-10 16:17:10 -0700

    Merged functions and cleaned up code

--- example.c
+++ example.c
@@ -5,7 +5,7 @@
 #include "complex.h"
 
 void fft(complex float* x, complex float* X, int N);
-void fft(complex float* X, complex float* x, int N);
+void ifft(complex float* X, complex float* x, int N);
 
 
 int main(){
@@ -13,8 +13,22 @@ int main(){
   complex float X[8];
   int k;
   x[0]=1; x[1]=2; x[2]=3; x[3]=4; x[4]=5; x[5]=6; x[6]=7; x[7]=8;
-  fft(x,X,5);
-  ifft(X,x,5);
+
+  printf("Original signal:\n");
+
+  for(k=0; k<5; ++k)
+    printf("%f j%f\n",creal(x[k]),cimag(x[k]));
+  
+  fft(x,X,5); /* FFT */
+
+  printf("\nFFT of signal:\n");
+
+  for(k=0; k<5; ++k)
+    printf("%f j%f\n",creal(X[k]),cimag(X[k]));
+  
+  ifft(X,x,5); /* Inverse FFT */
+  
+  printf("\nInverse FFT of FFT of signal:\n");
   for(k=0; k<5; ++k)
     printf("%f j%f\n",creal(x[k]),cimag(x[k]));
   return 0;
--- fft.c
+++ fft.c
@@ -4,68 +4,22 @@
 #include "math.h"
 #include "complex.h"
 
-/*
-void dft(complex float* x, complex float* X, int N){
-  int n,k;
-  complex float s,z;
-  z=0;
-  for(k=0; k<N; ++k){
-    //fprintf(stderr,"%d %d\n",k,N);
-    s=0;
-    z=-2*M_PI*I*k/N;
-    for(n=0; n<N; ++n){
-      s+=x[n]*cexp(z*n);
-    }
-    X[k]=s;
-  }
-  return;
-}
-*/
-
-void fft_depth(complex float* x, complex float* X, int N, int depth,int Nr){
-  complex float* coeffs;
-  complex float s;
-  complex float s1;
-  complex float z;
-  int i,j,k;
-  int p2d,N_over_p2d;
-  p2d=(int) pow(2,depth);
-  N_over_p2d=N/p2d;
-  coeffs=(complex float*) malloc(sizeof(complex float)*N/pow(2,depth));
-  for(k=0; k<N; ++k){
-    //fprintf(stderr,"%d %d\n",k,N);
-    z=-2*M_PI*I*k/Nr;
-    //z=-2*M_PI*I*k*p2d/N;
-    for(i=0; i<N/pow(2,depth); ++i){
-      //coeffs[i]=cexp(z*i*pow(2,depth));
-      coeffs[i]=cexp(z*i*p2d);
-    }
-    
-    s=0;
-    for(i=0; i<p2d; ++i){
-      s1=0;
-      for(j=0; j<N_over_p2d; ++j){
-	//s1+=coeffs[j]*x[ (int) (j*pow(2,depth)+i)];
-	//s1+=cexp(z*j*p2d)*x[ (int) (j*p2d+i)];
-	s1+=coeffs[j]*x[ (int) (j*p2d+i)];
-      }
-      s+=s1*cexp(z*i);
-    }
-    X[k]=s;
-  }
-  
-  return;
-}
-
 void fft(complex float* x, complex float* X, int N){
   complex float* xp;
   complex float* Xp;
   int depth;
   int i;
   int Np;
+  
+  complex float* coeffs;
+  complex float s;
+  complex float s1;
+  complex float z;
+  int j,k;
+  int p2d,N_over_p2d;
+  
   for(i=0; pow(2,i)<N; ++i);
   Np=pow(2,i);
-  printf("%d %d\n", N, Np);
   xp=(complex float*) malloc(Np*sizeof(complex float));
   Xp=(complex float*) malloc(Np*sizeof(complex float));
   for(i=0; i<N; ++i)
@@ -73,14 +27,33 @@ void fft(complex float* x, complex float* X, int N){
   for(i=N; i<Np; ++i)
     xp[i]=0;
   depth=(int) (log(Np)/log(2))/2;
-  fft_depth(xp, Xp, Np, depth,N);
+
+  p2d=(int) pow(2,depth);
+  N_over_p2d=Np/p2d;
+  coeffs=(complex float*) malloc(sizeof(complex float)*Np/pow(2,depth));
+  for(k=0; k<N; ++k){
+    z=-2*M_PI*I*k/N;
+    for(i=0; i<Np/pow(2,depth); ++i){
+      coeffs[i]=cexp(z*i*p2d);
+    }
+    
+    s=0;
+    for(i=0; i<p2d; ++i){
+      s1=0;
+      for(j=0; j<N_over_p2d; ++j){
+	s1+=coeffs[j]*xp[ (int) (j*p2d+i)];
+      }
+      s+=s1*cexp(z*i);
+    }
+    Xp[k]=s;
+  }
+  
   for(i=0; i<N; ++i)
     X[i]=Xp[i];
 }
 
 void ifft(complex float* X, complex float* x, int N){
   complex float* xr;
-  //int depth=(int) (log(N)/log(2))/2;
   int i;
   xr=(complex float*) malloc(sizeof(complex float)*N);
   fft(X, xr, N);
